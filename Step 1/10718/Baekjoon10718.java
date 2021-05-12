8f1641d1efe197cbb01e47bffa6f65490db12905
YoungHyunB <qodudgus0220@naver.com>
2021-05-12 23:48:33 +0900

    step 1

---
+++ Step 1/10718/Baekjoon10718.java
@@ -0,0 +1,9 @@
+package Step1;
+
+public class Baekjoon10718 {
+	public static void main(String[] args) {
+		// 두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}
---
+++ Step 1/2557/Baekjoon2557.java
@@ -0,0 +1,8 @@
+package Step1;
+
+public class Baekjoon2557 {
+	public static void main(String[] args) {
+		// Hello World!를 출력하시오.
+		System.out.println("Hello World!");
+	}
+}
